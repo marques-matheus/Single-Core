0aa79b22b173c0ac40b03065134279cd3e3c0b4e
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2024-11-08 19:04:26 -0300

    fix: cardSuporte

--- src/app/Cells/cardSuporte/CardSuporte.tsx
+++ src/app/Cells/cardSuporte/CardSuporte.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ApartmentOutlinedIcon from '@mui/icons-material/ApartmentOutlined';
 import PolylineOutlinedIcon from '@mui/icons-material/PolylineOutlined';
 import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
@@ -11,27 +11,34 @@ const CardSuporte = () => {
     ]);
 
     const setIsOpen = (index: number, isOpen: boolean) => {
-        setCards((prevCards) =>
-            prevCards.map((card, i) => (i === index ? { ...card, isOpen } : card))
-        );
+        setCards((prevCards) => {
+            if (!prevCards || !Array.isArray(prevCards)) {
+                console.error('Invalid cards state');
+                return prevCards;
+            }
+            return prevCards.map((card, i) => 
+                (i === index && card) ? { ...card, isOpen } : card
+            );
+        });
     };
+    
     return (
-        <div className="flex w-4/6 m-auto relative my-3">
+        <div className="flex xl:flex-row flex-col w-4/6 m-auto relative my-3">
             {cards.map((card, index) => (
                 <article
                     key={index}
-                    className={`w-1/3 relative py-5 px-9 bg-cyan-800 h-96 hover:w-full transition-all ease-in-out duration-300 text-center justify-center items-center flex mx-1 ${card.isOpen ? "w-full" : "w-32"}
+                    className={`w-1/3 relative py-5 px-9 bg-blue-900 xl:h-96 h-54  w-full xl:my-0 my-0.5 hover:w-full transition-all ease-in-out duration-300 text-center justify-center items-center flex mx-1 ${card.isOpen ? "xl:w-full" : "xl:w-1/3"}
                         }`}
                     onMouseOver={() => setIsOpen(index, true)}
                     onMouseOut={() => setIsOpen(index, false)}
                 >
                     <div className="flex flex-col justify-center items-center">
-                        <div className={`${card.isOpen ? "hidden" : "block"} text-white transition-all duration-200 ease-in-out text-5xl`}>
+                        <div className={`${card.isOpen ? "hidden opacity-0" : "block opacity-100 ease-in-out transition-all duration-500"} text-white  text-5xl`}>
                             {card.icon}
                         </div>
-                        <h1 className={`${card.isOpen ? "hidden" : "block"} text-3xl font-bold text-white transition-all duration-200 ease-in-out`}>	{card.title}</h1>
+                        <h1 className={`${card.isOpen ? "hidden opacity-0" : "block opacity-100 ease-in-out transition-all duration-500"} text-3xl font-bold text-white `}>	{card.title}</h1>
                     </div>
-                    <p className={`${card.isOpen ? "block" : "hidden"} text-white transition-all duration-200 ease-in-out font-medium`}>{card.descricao}</p>
+                    <p className={`${card.isOpen ? "block opacity-100 ease-in-out transition-all duration-500" : "hidden opacity-0" } text-white  font-medium`}>{card.descricao}</p>
                 </article>
             ))}
         </div>
