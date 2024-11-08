b801ac969e6da7613fb5a5a42593987dfd1ac05f
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2024-11-08 19:57:30 -0300

    fix:ajuste de texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
     <>
       <Header />
       <Banner>
-        <main className="w-full h-full flex items-center justify-between my-32">
-          <article className="max-w-xl m-auto">
-            <h1 className="text-5xl font-bold text-sc-700 my-3">Soluções em TI com o melhor custo benefício</h1>
-            <p className="text-xl font-light text-sc-700 leading-6">Se você busca por agilidade no atendimento e a melhor prestação de serviço em TI do mercado chegou ao lugar certo. Nós da Single Core temos o que busca com o melhor valor!</p>
+        <main className="w-full h-full flex items-center justify-between my-28">
+          <article className="max-w-xl bg-opacity-75 lg:bg-transparent bg-white p-8 lg:m-auto ">
+            <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold text-sc-700 my-3">Soluções em TI com o melhor custo benefício</h1>
+            <p className="lg:text-xl font-light text-sc-700 leading-6">Se você busca por agilidade no atendimento e a melhor prestação de serviço em TI do mercado chegou ao lugar certo. Nós da Single Core temos o que busca com o melhor valor!</p>
           </article>
           <article>
             <Image src="/image.png" className="hidden lg:block" width={600} height={150} alt="logo" />
