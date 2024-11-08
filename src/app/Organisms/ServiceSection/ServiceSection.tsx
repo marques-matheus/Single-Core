2b29a07c5fb2d78375046c12b068d828c40bbbb5
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2024-11-08 15:02:24 -0300

    fix:style: fix grid

--- src/app/Organisms/ServiceSection/ServiceSection.tsx
+++ src/app/Organisms/ServiceSection/ServiceSection.tsx
@@ -5,7 +5,7 @@ const ServiceSection = () => {
     return (
         <section className="flex flex-col items-center p-5">
             <Heading text="Nossos profissionais estão capacitados para solucionar o seu problema." textSub="Serviços" align="text-center" />
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 my-5">
                 <ServiceCard />
             </div>
         </section>
