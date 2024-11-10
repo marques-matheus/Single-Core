253dbf91fb3ec8371091c3a3e569fc330d9d3c05
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2024-11-10 14:48:39 -0300

    feat: MVV section

--- src/app/Atoms/heading/Heading.tsx
+++ src/app/Atoms/heading/Heading.tsx
@@ -1,7 +1,7 @@
 
 const Heading = ({ text, textSub, align }: { text: string, textSub?: string, align?: string }) => {
     return (
-        <div className="flex flex-col my-5 space-y-4 items-center justify-center ">
+        <div className={`flex flex-col my-2 space-y-4 ${align == "text-left" ? "items-start" : "items-center justify-center"}`}>
             <sub className={`text-sc-700 font-semibold text-lg ${align}`}>{textSub}</sub>
             <h2 className={`text-xl font-bold max-w-sm ${align}`}>{text}</h2>
         </div>
---
+++ src/app/Atoms/mvv/Mvv.tsx
@@ -0,0 +1,36 @@
+import { ChevronsDown } from "react-feather"
+
+const texts = [
+    {
+        id: 1,
+        title: 'Missão',
+        description: 'Levar os serviços de TI e Telecom a empresas com demandas baixas e complexas, com o melhor custo-benefício e sem a necessidade de uma contratação CLT.'
+    },
+    {
+        id: 2,
+        title: 'Visão',
+        description: 'Somos parceiros, acreditamos na tecnologia como forma de construção e firmamento de marcas. Oferecemos a evolução do seu negócio.'
+    },
+    {
+        id: 3,
+        title: 'Valores',
+        description: 'Confiança, segurança e lealdade são os diferenciais do sucesso de nossa empresa, com o atendimento humanizado entendemos quais as suas necessidades e apresentamos as melhores soluções.'
+    },
+]
+
+
+const Mvv = () => {
+    return (
+        <article className="flex flex-col  space-y-5 w-full lg:max-w-xl">
+            {texts.map((text, index) => (
+                <div key={index} className="flex flex-col p-3 space-y-2  border-b-2 border-sc-600">
+                    <h2 className="text-sc-600 text-xl   font-bold">{text.title}</h2>
+                    <p className=" font-light text-sm lg:text-lg ">{text.description}</p>
+                </div>
+            ))}
+        </article>
+
+    )
+}
+
+export default Mvv
